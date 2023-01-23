03869a92bb4344c6da40d5e6a335bc814d11f3ca
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-01-23 18:53:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap all odd and even bits/swap-all-odd-and-even-bits.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    //Function to swap odd and even bits.
+    public static int swapBits(int n) 
+    {
+	    // Your code
+	      int x=n&0x55555555;//number with all odd bits as set
+        int y=n&0xAAAAAAAA;//number with all even bits as set
+        y=y>>1; //right shift even bits
+        x=x<<1;//left shift odd bits
+        return(x|y); //return combined result
+	}
+    
+}
+
+//{ Driver Code Starts.
+
+class BitWise{
+
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//taking n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling swapBits() method of class
+		    //Swap
+		    System.out.println(obj.swapBits(n));
+		}
+	}
+}
+// } Driver Code Ends
