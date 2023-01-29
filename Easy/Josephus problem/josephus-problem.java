f3c6a0313be19cdaf3c1073b9c40294fe70773ce
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-01-29 22:20:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Josephus problem/josephus-problem.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Recursion {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();//testcases
+		while(T-->0)
+		{
+		    Solution obj=new Solution();
+		    
+		    int n,k;
+		    //taking input n and k
+		    n=sc.nextInt();
+		    k=sc.nextInt();
+		    
+		    //calling josephus() function
+		    System.out.println(obj.josephus(n,k));
+		    
+		    
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+   public int josephus(int n, int k)
+    {
+        //Your code here
+       int prev = 1;
+       for(int i=2;i<=n;i++) 
+       {
+           int x = prev + k;
+           prev = x%i == 0?i:x%i;
+       }
+       return prev;
+    }
+
+}
+
