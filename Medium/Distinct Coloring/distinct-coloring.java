6dc303a8e8b6a948aed43a948c8552600cf54ed4
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-01 21:36:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Distinct Coloring/distinct-coloring.java
+++ Medium/Distinct Coloring/distinct-coloring.java
@@ -18,15 +18,17 @@ class Solution{
         dp[0][1]=g[0];
         dp[0][2]=b[0];
         
-        for(int i=1;i<N;i++){
+        for(int i=1;i<N;i++)
+        {
             dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+r[i];
             dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+g[i];
             dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+b[i];
         }
-        return Math.min(dp[N-1][0],Math.min(dp[N-1][1],dp[N-1][2]));
+        long ans1 =Math.min(dp[N-1][1],dp[N-1][2]);
+        long ans= Math.min(dp[N-1][0], ans1);
+        return ans;
     }
 }
-
 
 //{ Driver Code Starts.
 
