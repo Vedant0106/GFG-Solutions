376f6445b6f20d6c37d6e250e3bc7dd3de7a3900
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-05 00:24:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implement Atoi/implement-atoi.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.Scanner;
+
+class aToi
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String str = sc.nextLine();
+			
+			Solution obj = new Solution();
+			System.out.println(obj.atoi(str));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/*You are required to complete this method */
+class Solution
+{
+    int atoi(String str) 
+    {
+	// Your code here
+	  try
+	  { 
+	      int a = Integer.parseInt(str);
+          return a;
+      }
+      catch(NumberFormatException bce)
+      {
+        return -1;
+      }
+    }
+}
