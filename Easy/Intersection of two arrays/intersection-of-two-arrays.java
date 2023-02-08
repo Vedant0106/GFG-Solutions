e28da7b4fb0873816f6dd390f820d7bd37672ec4
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-09 00:47:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two arrays/intersection-of-two-arrays.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code here
+        int count=0;
+        HashSet<Integer> set= new HashSet<>();
+        for(int val: a)
+        {
+            set.add(val);
+        }
+        for(int val:b)
+        {
+            if(set.contains(val))
+            {
+             count++;
+             set.remove(val);
+            }
+       }
+    return count;
+    }
+};
