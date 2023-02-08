61a1834571176c00a459bd06424e189cc00a855e
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-09 00:46:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Job first/shortest-job-first.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int solve(vector<int>& bt) {
+        // code here
+        sort(bt.begin(), bt.end());
+        int res = 0, t = 0;
+        for(int i : bt)
+        {
+            res += t;
+            t += i;
+        }
+        return res / bt.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> bt(n), p(n);
+        for (int i = 0; i < n; i++) {
+            cin >> bt[i];
+        }
+        Solution obj;
+        int ans = obj.solve(bt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
