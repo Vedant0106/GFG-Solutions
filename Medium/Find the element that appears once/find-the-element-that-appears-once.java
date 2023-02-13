aa7b43852d095e9cc481c10383f94c53cde88b9d
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-14 01:25:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the element that appears once/find-the-element-that-appears-once.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; ++i)
+                arr[i] = sc.nextInt ();
+
+    		System.out.println (new Sol().search (arr, n));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    public static int search(int A[], int N)
+    {
+        // your code here
+         int count =0;
+        for(int i=0;i<N;i++){
+           count=count^A[i];
+        }
+        return count;
+    }
+}
