4c9ccca7940583e147bb4e1f61de69b39c837422
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-02-28 13:44:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Optimal Array/optimal-array.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(in.readLine());
+            String s[]=in.readLine().trim().split(" ");
+            int a[]=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=Integer.parseInt(s[i]);
+            }
+            Solution ob=new Solution();
+            long arr[]=ob.optimalArray(n,a);
+            for(long i:arr){
+                out.print(i+" ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public long[] optimalArray(int n,int a[])
+    {
+      long[] ans = new long[n];
+      ans[0] = 0;
+      for(int i = 1; i < n; i++) 
+      {
+            ans[i] = a[i]-a[i/2] + ans[i-1];
+      }
+    return ans;   
+    }
+}
