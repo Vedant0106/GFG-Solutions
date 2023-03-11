22547e2f62d7778c3feb0e1ad400698304a96beb
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-03-11 18:34:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Yet another query problem/yet-another-query-problem.java
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int num;
+            num = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            
+            int[][] Q = IntMatrix.input(br, num, 3);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.solveQueries(N, num, A, Q);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    public static ArrayList<Integer> solveQueries(int N, int num, int[] A, int[][] Q) {
+
+        
+
+        ArrayList<Integer> list  = new ArrayList<>();
+
+        
+
+        for(int i=0; i< Q.length; i++){
+
+            int left = Q[i][0];
+
+            int right = Q[i][1];
+
+            int K = Q[i][2];
+
+            int count = 0;
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            
+
+            for(int j=left; j<N; j++) map.put(A[j], map.getOrDefault(A[j],0)+1);
+
+            count =0;
+
+            for(int j=left; j<= right; j++){
+
+                if(map.get(A[j]) == K) count++;
+
+                if(map.get(A[j]) == 1) map.remove(A[j]);
+
+                else map.put(A[j], map.get(A[j])-1);
+
+            }
+
+            list.add(count); 
+
+        }
+
+        
+
+        return list;
+
+    }
+
+ 
+
+}
+
