d747668568590000710445a9b6e58f8bc56f717c
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-03-12 14:52:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Binary matrix having maximum number of 1s/binary-matrix-having-maximum-number-of-1s.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class GfG {
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int B = sc.nextInt();
+            int arr[][] = new int[B][B];
+            for(int i = 0; i < B; i++) {
+                for(int j = 0; j < B; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            Solution obj = new Solution();
+            int ans[] = obj.findMaxRow(arr, B);
+            for(int i = 0; i < 2; i++) {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int[] findMaxRow(int mat[][], int N) {
+        // code here
+         int[] ans=new int[2];
+        int row=-1,maxi=-1;
+        for(int[] arr:mat){
+            row+=1;
+            int index=startIndex(arr);
+            if(index!=-1 ){
+                int number=N-index;
+                if(number>maxi){
+                    maxi=number;
+                    ans[0]=row;
+                    ans[1]=number;
+                }
+            }
+        }
+        return ans;
+    }
+    private int startIndex(int[] arr){
+        int index=-1;
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==1){
+                index=mid;
+                if(mid-1>=0 && arr[mid-1]==1){
+                    start=0;
+                    end=mid-1;
+                }else{
+                    return index;
+                }
+            }else{
+                start=mid+1;
+            }
+        }
+        return index;
+    }
+};
