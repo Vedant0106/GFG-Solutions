16161f9b0252b334835d0fd7b710889602bee8a8
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-04-26 18:20:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equal Left and Right Subarray Sum/equal-left-and-right-subarray-sum.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int [] A = new int[N];
+			String [] str = br.readLine().trim().split(" ");
+			for(int i = 0; i < N; i++)
+				A[i] = Integer.parseInt(str[i]);
+			Solution ob = new Solution();
+			System.out.println(ob.equalSum(A, N));
+		}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	int equalSum(int [] A, int N) {
+		//Write your code here
+		  int sum=0; 
+
+  for(int i=0;i<N;i++)sum+=A[i]; 
+
+  int pre=0; 
+
+  for(int i=0;i<N;i++){ 
+
+      sum-=A[i]; 
+
+      if(pre==sum) return i+1;
+
+      pre+=A[i]; 
+
+  }
+
+  return -1;
+	}
+}
