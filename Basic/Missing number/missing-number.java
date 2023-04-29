c1d7d702addc1a99660a239b816715313b28f529
VEDANT SRIVASTAVA <85980753+Vedant0106@users.noreply.github.com>
2023-04-29 19:51:31 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Missing number/missing-number.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n+5];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n-1; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.missingNumber(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public static int missingNumber(int A[], int N)
+    {
+         // Your code goes here
+         int num = (N*(N+1))/2;
+         int sum=0;
+         for(int i=0;i<A.length;i++){
+             sum+=A[i];
+         }
+         return num - sum;
+    }
+}
